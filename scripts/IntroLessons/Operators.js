0cf592d8a065136572c2bc697a440c54e4e1ade4
Kurtis Payne <45240824+Scrapperfri3z@users.noreply.github.com>
2019-09-20 12:51:02 -0600

    sure

---
+++ scripts/IntroLessons/Operators.js
@@ -0,0 +1,28 @@
+console.log("Operators You made it");
+
+function sinmpleOperators (numberA, numberB){
+  let adding = numberA + numberB;
+  let subtraction = numberA - numberB;
+  let mulitplying = numberA * numberB;
+  let division = numberA / numberB;
+  let modulus = numberA % numberB;
+  console.log(numberA, numberB, 'adding', adding);
+  console.log(numberA, numberB, 'subtraction',subtraction);
+  console.log(numberA, numberB, 'mulitplying', mulitplying);
+  console.log(numberA, numberB, 'division',division);
+  console.log(numberA, numberB, 'modulus',modulus);
+}
+
+skin count by 4 for 10 times starting at 69
+function skipCointing (interval) {
+
+}
+
+let number = [56, 101, 232, 1, 12, 23, 45, 645, '1'];
+
+
+
+
+sinmpleOperators(number[0], number[5]);
+sinmpleOperators(number[2], number[3]);
+sinmpleOperators(number[3], number[7]);
